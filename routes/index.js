f55ed739ee4c49f3a2ab0a8cd159c84a2e0852fd
ravi kumar <coolravikumar2050@gmail.com>
2020-10-19 20:41:41 +0530

    v9

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.get('/set-cookie', function(req, res, next) {
 
 
 router.get('/check-cookie', function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
